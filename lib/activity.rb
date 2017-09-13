ad2a9fb705f4c9e5483a5ecce7d59ca3faf2f3d7
Marshall Houston <marshall.houston@gmail.com>
2017-09-13 09:41:46 -0600

    complete iteration 1

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,19 @@
 class Activity
+attr_reader :name, :participants
+
+
+  def initialize(name, participant)
+    @name = name
+    @participants = {}
+      participant.each do |k,v|
+        @participants[k] = v
+      end
+  end
+
+  def total_cost
+    @participants.reduce(0) do |sum, participant|
+      sum + participant[1]
+    end
+  end
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -1,7 +1,33 @@
+# Add to the existing Activity class so that it supports the following functionality.
+#
+# * An Activity has a name
+# * An Activity has participants
+#   * participants are represented with a hash and have a name and an amount they've paid for that activity
+# * You can get the total cost of an activity
+
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def test_it_has_a_name
+    activity = Activity.new("fishing", {'Steve' => 25})
+
+    assert_equal "fishing", activity.name
+  end
+
+  def test_it_can_have_participants
+    activity = Activity.new("fishing", {'Steve' => 25, 'Alejandra' => 24, 'Amy' => 26})
+
+    assert_equal 3, activity.participants.count
+  end
+
+  def test_it_can_calculate_total_cost
+    activity = Activity.new("fishing", {'Steve' => 25, 'Alejandra' => 24, 'Amy' => 26})
+
+    assert_equal 75, activity.total_cost
+  end
+
 end
